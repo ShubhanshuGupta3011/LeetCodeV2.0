07e8f522f135dd363ec9fa7154114eaff92c5275
Shubhanshu Gupta <109612830+ShubhanshuGupta3011@users.noreply.github.com>
2025-11-26 01:51:28 +0530

    Time: 3 ms (41.73%) | Memory: 152.8 MB (21.95%) - LeetSync

---
+++ 3950-maximum-k-to-sort-a-permutation/maximum-k-to-sort-a-permutation.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int sortPermutation(vector<int>& nums) {
+        int ans=INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=i){
+                ans&=i;
+            }
+        }
+        return ans==INT_MAX?0:ans;
+    }
+};
