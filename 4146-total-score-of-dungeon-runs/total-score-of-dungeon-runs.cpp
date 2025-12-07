11d7ae28017521a9f904a6ea66573ed507ff2812
Shubhanshu Gupta <109612830+ShubhanshuGupta3011@users.noreply.github.com>
2025-12-07 14:41:01 +0530

    Time: 49 ms (45.03%) | Memory: 266.5 MB (26.97%) - LeetSync

---
+++ 4146-total-score-of-dungeon-runs/total-score-of-dungeon-runs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long totalScore(int h, vector<int>& d, vector<int>& k){
+        long long z = h;
+        long long ans = 0, s = 0;
+        vector<long long> p = {0};
+        for (int x : d)
+            p.push_back(s += x);
+        for (int i = 0; i < d.size(); ++i) {
+            long long t = p[i + 1] + k[i] - z;
+            int ip = lower_bound(p.begin(), p.end(), t) - p.begin();
+            if (ip <= i) ans += i - ip + 1;
+        }
+        return ans;
+    }
+};
