28265737e02ac4e3f33e5a7def04a0c7048dce54
Shubhanshu Gupta <109612830+ShubhanshuGupta3011@users.noreply.github.com>
2025-12-10 05:06:23 +0530

    Time: 91 ms (20.06%) | Memory: 182.3 MB (46.58%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -1,39 +1,47 @@
 class NumArray {
 public:
-    vector<int> ft = {0};
-    NumArray(vector<int>& nums) {
-        for(int i=0;i<nums.size();i++){
-            ft.push_back(0);
+    vector<int> seg;
+    void build(int node,int left,int right,vector<int>& nums){
+        if(left==right){
+            seg[node]=nums[left];
+            return ;
         }
-        for(int i=0;i<nums.size();i++){
-            int j=i+1;
-            while(j<ft.size()){
-                ft[j]+=nums[i];
-                j+=(j&(-j));
-            }
-        }
+        int mid=(left+right)/2;
+        build(2*node+1,left,mid,nums);
+        build(2*node+2,mid+1,right,nums);
+        seg[node]=seg[2*node+1]+seg[2*node+2];
     }
-    int helper(int index){
-        int ans=0;
-        while(index){
-            ans+=ft[index];
-            index-=(index&(-index));
-        }
-        return ans;
+    int getSum(int node,int left,int right,int l,int r){
+        if(left>=l && right<=r) return seg[node];
+        if(right<l || r<left) return 0;
+        int mid=(left+right)/2;
+        return getSum(2*node+1,left,mid,l,r)+getSum(2*node+2,mid+1,right,l,r);
+    }
+    NumArray(vector<int>& nums) {
+        int n=nums.size();
+        seg.resize(4*n);
+        build(0,0,n-1,nums);
+    }
+
+    int sumRange(int left, int right) {
+        int n=seg.size()/4;
+        return getSum(0,0,n-1,left,right);
+    }
+    void updating(int node,int left,int right,int index,int diff){
+        if(index < left || index > right) return;   
+        seg[node] += diff;
+        if(left == right) return;
+        int mid = (left+right)/2;
+        if(index <= mid) updating(2*node+1,left,mid,index,diff);
+        else updating(2*node+2,mid+1,right,index,diff);
     }
     void update(int index, int val) {
-        int curr = helper(index+1)-helper(index);
+        int n=seg.size()/4;
+        int curr = sumRange(0,index)-(index?sumRange(0,index-1):0);
         int diff = val-curr;
-        index++;
-        while(index<ft.size()){
-            ft[index]+=diff;
-            index += (index&(-index));
-        }
+        updating(0,0,n-1,index,diff);
     }
     
-    int sumRange(int left, int right) {
-        return helper(++right)-helper(++left-1);
-    }
 };
 
 /**
