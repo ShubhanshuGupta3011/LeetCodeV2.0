2756c58834dd65803a9f75bc54b79f6a1835f9c5
Shubhanshu Gupta <109612830+ShubhanshuGupta3011@users.noreply.github.com>
2025-12-11 06:11:15 +0530

    Time: 1594 ms (8.84%) | Memory: 484.2 MB (5.52%) - LeetSync

--- 3819-count-covered-buildings/count-covered-buildings.cpp
+++ 3819-count-covered-buildings/count-covered-buildings.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
-        map<int,vector<int>> xaxis,yaxis;
+        map<int,set<int>> xaxis,yaxis;
         for(auto it:buildings){
             int x=it[0];
             int y=it[1];
-            xaxis[x].push_back(y);
-            yaxis[y].push_back(x);
-        }
-        for(auto it:xaxis){
-            int key=it.first;
-            sort(xaxis[key].begin(),xaxis[key].end());
-        }
-        for(auto it:yaxis){
-            int key=it.first;
-            sort(yaxis[key].begin(),yaxis[key].end());
+            xaxis[x].insert(y);
+            yaxis[y].insert(x);
         }
         int ans=0;
         for(auto it:buildings){
@@ -22,8 +14,8 @@ public:
             int y=it[1];
             if(xaxis[x].size()<3) continue;
             if(yaxis[y].size()<3) continue;
-            if(xaxis[x][0]==y || xaxis[x].back()==y) continue;
-            if(yaxis[y][0]==x || yaxis[y].back()==x) continue;
+            if(*xaxis[x].begin()==y || *xaxis[x].rbegin()==y) continue;
+            if(*yaxis[y].begin()==x || *yaxis[y].rbegin()==x) continue;
             ans++;
         }
         return ans;
