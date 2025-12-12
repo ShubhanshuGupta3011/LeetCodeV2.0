0efb7410bb277d1a020ed83d0b49540eb93f0064
Shubhanshu Gupta <109612830+ShubhanshuGupta3011@users.noreply.github.com>
2025-12-12 15:06:05 +0530

    Time: 107 ms (40.07%) | Memory: 159.2 MB (26.47%) - LeetSync

---
+++ 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/count-the-number-of-arrays-with-k-matching-adjacent-elements.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int mod=1e9+7;
+    vector<int> fact;
+    int multi(int a,int b){
+        return (1ll*a*b)%mod;
+    }
+    int power(int x,int n){
+        if(n<2) return n?x:1;
+        return multi(power(x,n%2),power(multi(x,x),n/2));
+    }
+    int inverse(int x){
+        return power(x,mod-2);
+    }
+    int countGoodArrays(int n, int m, int k) {
+        fact.push_back(1);
+        for(int i=1;i<=n;i++){
+            fact.push_back(multi(i,fact[i-1]));
+        }
+        int res = m;
+        res=multi(res,power(m-1,n-k-1));
+        res = multi(res,fact[n-1]);
+        res = multi(res,inverse(fact[k]));
+        res = multi(res,inverse(fact[n-k-1]));
+        return res;
+    }
+};
