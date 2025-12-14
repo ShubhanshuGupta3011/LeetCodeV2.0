1da3b1784bf132df283d7da646bea93338a64e92
Shubhanshu Gupta <109612830+ShubhanshuGupta3011@users.noreply.github.com>
2025-12-14 18:56:23 +0530

    Time: 587 ms (12.83%) | Memory: 247.3 MB (20.51%) - LeetSync

--- 4063-count-distinct-subarrays-divisible-by-k-in-sorted-array/count-distinct-subarrays-divisible-by-k-in-sorted-array.cpp
+++ 4063-count-distinct-subarrays-divisible-by-k-in-sorted-array/count-distinct-subarrays-divisible-by-k-in-sorted-array.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
     long long numGoodSubarrays(vector<int>& nums, int k) {
-        /*
-        1 0 0
-        */
-        int n=nums.size();
-        reverse(nums.begin(),nums.end());
-        vector<int> temp=nums;
-        map<int,int> mp;
-        map<pair<int,int>,int> mp2;
-        mp[0]=1;
-        mp2[{0,-1}]=1;
+        int n = nums.size();
+        reverse(nums.begin(), nums.end());
+        vector<int> temp = nums;
+        map<int, int> mp;
+        map<pair<int, int>, int> mp2;
+        mp[0] = 1;
+        mp2[{0, -1}] = 1;
         int prefixSum = 0;
-        long long ans=0;
-        for(int i=0;i<n;i++){
-            prefixSum+=nums[i];
-            prefixSum=prefixSum%k;
-            // cout<<"prefixSum = "<<prefixSum;
-            // cout<<" nums["<<i<<"] = "<<nums[i];
-            int myCount=mp2[{prefixSum,nums[i]}];
-            int leftCount=mp[prefixSum];
-            // cout<<" myCount = "<<myCount;
-            // cout<<" leftCount = "<<leftCount<<endl;
-            ans+=leftCount;
-            ans-=myCount;
+        long long ans = 0;
+        for (int i = 0; i < n; i++) {
+            prefixSum += nums[i];
+            prefixSum = prefixSum % k;
+            int myCount = mp2[{prefixSum, nums[i]}];
+            int leftCount = mp[prefixSum];
+            ans += leftCount;
+            ans -= myCount;
             mp[prefixSum]++;
-            mp2[{prefixSum,nums[i]}]++;
+            mp2[{prefixSum, nums[i]}]++;
         }
-        /*
-        0  2 4 0 2 4 0
-        */
         return ans;
     }
 };
