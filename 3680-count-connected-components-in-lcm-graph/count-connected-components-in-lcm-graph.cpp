61223e5144a8ccb730bf1fc3fa94a70790b54e93
Shubhanshu Gupta <109612830+ShubhanshuGupta3011@users.noreply.github.com>
2026-01-11 21:37:46 +0530

    Time: 1216 ms (18.12%) | Memory: 242.4 MB (14.49%) - LeetSync

--- 3680-count-connected-components-in-lcm-graph/count-connected-components-in-lcm-graph.cpp
+++ 3680-count-connected-components-in-lcm-graph/count-connected-components-in-lcm-graph.cpp
@@ -1,72 +1,77 @@
 class Solution {
 public:
-    long long Threshold ;
-    map<int,int> divisor;
-    map<int,int> getValueFromIndex;
-    vector<int> parent;
-    int findParent(int a){
-        if(a==parent[a]) return a;
-        return parent[a] = findParent(parent[a]);
+    long long Th;
+    map<int, int> d;
+    map<int, int> g;
+    vector<int> p;
+    int fp(int a) {
+        if (a == p[a])
+            return a;
+        return p[a] = fp(p[a]);
     }
-    void unioned(int a,int b){
-        a=findParent(a);
-        b=findParent(b);
-        parent[b]=a;
+    void un(int a, int b) {
+        a = fp(a);
+        b = fp(b);
+        p[b] = a;
     }
-    long long gcd(long long a,long long b){
-        if(a%b==0) return b;
-        return gcd(b,a%b);
+    long long gcd(long long a, long long b) {
+        if (a % b == 0)
+            return b;
+        return gcd(b, a % b);
     }
-    long long lcm(long long a,long long b){
-        long long hcf = gcd(a,b);
-        return (a/hcf)*(b);
+    long long lcm(long long a, long long b) {
+        long long hcf = gcd(a, b);
+        return (a / hcf) * (b);
     }
-    void helper(int value,int index){
+    void h(int value, int index) {
         set<int> fact = {1};
-        for(int i=1;i<=sqrt(value);i++){
-            if(value%i==0){
+        for (int i = 1; i <= sqrt(value); i++) {
+            if (value % i == 0) {
                 fact.insert(i);
-                fact.insert(value/i);
+                fact.insert(value / i);
             }
         }
-        for(auto it:fact){
-            if(divisor.find(it) != divisor.end()){
-                int beforeIndex = divisor[it];
-                int valueBefore = getValueFromIndex[beforeIndex];
-                int valueNow = getValueFromIndex[index];
-                if(lcm(valueBefore,valueNow) <= Threshold){
-                    unioned(beforeIndex,index);
+        for (auto it : fact) {
+            if (d.find(it) != d.end()) {
+                int beforeIndex = d[it];
+                int valueBefore = g[beforeIndex];
+                int valueNow = g[index];
+                if (lcm(valueBefore, valueNow) <= Th) {
+                    un(beforeIndex, index);
                 }
-            }else{
-                divisor[it]=index;
+            } else {
+                d[it] = index;
             }
         }
     }
-    int Component(){
+    int C() {
         set<int> st;
-        for(int i=0;i<parent.size();i++){
-            st.insert(findParent(i));
+        for (int i = 0; i < p.size(); i++) {
+            st.insert(fp(i));
         }
         return st.size();
     }
-    int countComponents(vector<int>& nums, int threshold) {
-        Threshold = threshold;
-        sort(nums.begin(),nums.end());
-        int ans1=0;
-        while(nums.size()){
-            if(nums.back() <= threshold) break;
-            nums.pop_back();
-            ans1++;
+    int countComponents(vector<int>& n, int th) {
+        Th = th;
+        sort(n.begin(), n.end());
+        int a = 0;
+        while (n.size()) {
+            if (n.back() <= th)
+                break;
+            n.pop_back();
+            a++;
         }
-        if(nums.size() == 0) return ans1;
-        if(nums[0] == 1) return 1+ans1;
-        for(int i=0;i<nums.size();i++){
-            getValueFromIndex[i] = nums[i];
-            parent.push_back(i);
+        if (n.size() == 0)
+            return a;
+        if (n[0] == 1)
+            return 1 + a;
+        for (int i = 0; i < n.size(); i++) {
+            g[i] = n[i];
+            p.push_back(i);
         }
-        for(int i=0;i<nums.size();i++){
-            helper(nums[i],i);
+        for (int i = 0; i < n.size(); i++) {
+            h(n[i], i);
         }
-        return ans1+Component();
+        return a + C();
     }
 };
