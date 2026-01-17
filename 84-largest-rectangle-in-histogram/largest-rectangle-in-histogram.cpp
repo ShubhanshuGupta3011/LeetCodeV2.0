3327645de79ee284172b17b8ca2469cf778b9d7c
Shubhanshu Gupta <109612830+ShubhanshuGupta3011@users.noreply.github.com>
2026-01-17 08:57:03 +0530

    Time: 41 ms (12.65%) | Memory: 85 MB (47.09%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,71 +1,28 @@
 class Solution {
 public:
-    vector<int> getLeftSmaller(vector<int>& heights,int n){
-        vector<int> ans(n,-1);
-        stack<int> st;
-        for(int i=0;i<n;i++){
-            while(st.size() && heights[i]<heights[st.top()]){
-                st.pop();
-            }
-            if(st.size()){
-                ans[i]=st.top();
-            }
-            st.push(i);
-        }
-        return ans;
-    }
-    vector<int> getRightSmaller(vector<int>& heights,int n){
-        vector<int> ans(n,n);
-        stack<int> st;
-        for(int i=0;i<n;i++){
-            while(st.size() && heights[i] < heights[st.top()]){
-                ans[st.top()]=i;
-                st.pop();
-            }
-            st.push(i);
-        }
-        return ans;
-    }
-    int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        // vector<int> leftSmaller = getLeftSmaller(heights,n);
-        // vector<int> rightSmaller = getRightSmaller(heights,n);
-        int ans=0;
-        // for(int i=0;i<n;i++){
-        //     ans = max(ans,heights[i] * (rightSmaller[i]-leftSmaller[i]-1));
-        // }
-        stack<pair<int,int>> st;
-        for(int i=0;i<n;i++){
-
-            while(st.size()){
-                int prevIndex = st.top().first;
-                int left = st.top().second;
-                int right = i;
-                if(heights[prevIndex] > heights[i]){
-                    ans = max(ans, heights[prevIndex] * (right - left - 1));
-                    st.pop();
-                }else{
-                    break;
-                }
-            }
-
-            if(st.empty()){
-                st.push({i,-1});
-            }else{
-                int prevIndex = st.top().first;
-                st.push({i,prevIndex});
-            }
-        }
-
+    void avoidDuplicated(stack<pair<int,int>>& st,vector<int>& heights,int i,int& ans){
         while(st.size()){
             int prevIndex = st.top().first;
             int left = st.top().second;
-            int right = n;
-            ans = max(ans, heights[prevIndex] * (right - left - 1));
-            st.pop();
+            int right = i;
+            if(i==heights.size() || heights[prevIndex] > heights[i]){
+                ans = max(ans, heights[prevIndex] * (right - left - 1));
+                st.pop();
+            }else{
+                break;
+            }
         }
-
-
+    }
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        int ans=0;
+        stack<pair<int,int>> st;
+        for(int i=0;i<n;i++){
+            avoidDuplicated(st,heights,i,ans);
+            int left = st.empty() ? -1 : st.top().first;
+            st.push({i,left});
+        }
+        avoidDuplicated(st,heights,n,ans);
         return ans;
     }
 };
