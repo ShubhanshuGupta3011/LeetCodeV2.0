285cc96147246383365656dc6ae77f1619f6c9a2
Shubhanshu Gupta <109612830+ShubhanshuGupta3011@users.noreply.github.com>
2026-01-22 22:59:07 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (57.18%) - LeetSync

--- 1332-count-vowels-permutation/count-vowels-permutation.cpp
+++ 1332-count-vowels-permutation/count-vowels-permutation.cpp
@@ -1,57 +1,40 @@
 class Solution {
 public:
-    int MOD = 1e9+7;
+    int MOD = 1e9 + 7;
     using T = vector<vector<int>>;
-    T I = {
-        {1,0,0,0,0},
-        {0,1,0,0,0},
-        {0,0,1,0,0},
-        {0,0,0,1,0},
-        {0,0,0,0,1}
-    };
-    T M = {
-        {0,1,0,0,0},
-        {1,0,1,0,0},
-        {1,1,0,1,1},
-        {0,0,1,0,1},
-        {1,0,0,0,0}
-    };
-    int add(int a,int b){
-        return (a+b) % MOD;
-    }
-    int multi(int a,int b){
-        return (1ll*a*b) % MOD;
-    }
-    T multi(T a,T b){
-        vector<vector<int>> ans(5,vector<int>(5,0));
-        for(int i=0;i<5;i++){
-            for(int j=0;j<5;j++){
-                for(int k=0;k<5;k++){
-                    ans[i][j] = add(ans[i][j],multi(a[i][k],b[k][j]));
-                }
-            }
-        }
+    T I = {{1, 0, 0, 0, 0},
+           {0, 1, 0, 0, 0},
+           {0, 0, 1, 0, 0},
+           {0, 0, 0, 1, 0},
+           {0, 0, 0, 0, 1}};
+    T M = {{0, 1, 0, 0, 0},
+           {1, 0, 1, 0, 0},
+           {1, 1, 0, 1, 1},
+           {0, 0, 1, 0, 1},
+           {1, 0, 0, 0, 0}};
+    int add(int a, int b) { return (a + b) % MOD; }
+    int multi(int a, int b) { return (1ll * a * b) % MOD; }
+    T multi(T a, T b) {
+        T ans(5, vector<int>(5, 0));
+        for (int i = 0; i < 5; i++)
+            for (int j = 0; j < 5; j++)
+                for (int k = 0; k < 5; k++)
+                    ans[i][j] = add(ans[i][j], multi(a[i][k], b[k][j]));
         return ans;
     }
-    T power(int n){
-        T ans = I;
-        T part = M;
-        while(n){
-            if(n%2) ans = multi(ans,part);
-            part = multi(part,part);
-            n=n/2;
-        }
-        return ans;
-        
+    T power(T A, int n) {
+        if (n < 2)
+            return n ? A : I;
+        T f = power(A, n % 2);
+        T s = power(multi(A, A), n / 2);
+        return multi(f, s);
     }
     int countVowelPermutation(int n) {
-        T res = power(n-1);
+        T res = power(M, n - 1);
         int ans = 0;
-        for(int i=0;i<5;i++){
-            for(int j=0;j<5;j++){
-                ans=add(ans,res[i][j]);
-            }
-        }
+        for (int i = 0; i < 5; i++)
+            for (int j = 0; j < 5; j++)
+                ans = add(ans, res[i][j]);
         return ans;
     }
 };
