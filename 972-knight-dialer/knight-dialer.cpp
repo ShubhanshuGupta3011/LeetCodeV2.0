db438ac82995c0bb7100a1f2f39128307ee9a0f1
Shubhanshu Gupta <109612830+ShubhanshuGupta3011@users.noreply.github.com>
2026-01-25 04:08:04 +0530

    Time: 43 ms (80.22%) | Memory: 25.2 MB (65.70%) - LeetSync

--- 972-knight-dialer/knight-dialer.cpp
+++ 972-knight-dialer/knight-dialer.cpp
@@ -1,32 +1,7 @@
 class Solution {
 public:
-
+    using T = vector<vector<int>>;
     int MOD = 1e9+7;
-    using T = vector<vector<int>> ;
-    T I = {
-        {1,0,0,0,0,0,0,0,0,0},
-        {0,1,0,0,0,0,0,0,0,0},
-        {0,0,1,0,0,0,0,0,0,0},
-        {0,0,0,1,0,0,0,0,0,0},
-        {0,0,0,0,1,0,0,0,0,0},
-        {0,0,0,0,0,1,0,0,0,0},
-        {0,0,0,0,0,0,1,0,0,0},
-        {0,0,0,0,0,0,0,1,0,0},
-        {0,0,0,0,0,0,0,0,1,0},
-        {0,0,0,0,0,0,0,0,0,1}
-    };
-    T M = {
-        {0,0,0,0,1,0,1,0,0,0},
-        {0,0,0,0,0,0,1,0,1,0},
-        {0,0,0,0,0,0,0,1,0,1},
-        {0,0,0,0,1,0,0,0,1,0},
-        {1,0,0,1,0,0,0,0,0,1},
-        {0,0,0,0,0,0,0,0,0,0},
-        {1,1,0,0,0,0,0,1,0,0},
-        {0,0,1,0,0,0,1,0,0,0},
-        {0,1,0,1,0,0,0,0,0,0},
-        {0,0,1,0,1,0,0,0,0,0}
-    };
     int add(int a,int b){
         return (a+b)%MOD;
     }
@@ -44,14 +19,26 @@ public:
         }
         return ans;
     }
-    T power(T a,int n){
+    T power(T a,int n,T I){
         if(n<2) return n?a:I;
-        T f = power(a,n%2);
-        T s = power(multi(a,a),n/2);
+        T f = power(a,n%2,I);
+        T s = power(multi(a,a),n/2,I);
         return multi(f,s);
     }
     int knightDialer(int n) {
-        T res = power(M,n-1);
+        vector<vector<int>> I(10,vector<int>(10,0));
+        vector<vector<int>> M(10,vector<int>(10,0));
+        for(int i=0;i<10;i++){
+            I[i][i]=1;
+        }
+        vector<vector<int>> connection = {
+            {0,4},{0,6},{1,6},{1,8},{2,7},{2,9},{3,4},{3,8},{4,9},{6,7}
+        };
+        for(auto it:connection){
+            M[it[0]][it[1]]=1;
+            M[it[1]][it[0]]=1;
+        }
+        T res = power(M,n-1,I);
         int ans=0;
         for(int i=0;i<10;i++){
             for(int j=0;j<10;j++){
