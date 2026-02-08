dea67596ff440452abe4a3a96702f7bc13c530cc
Shubhanshu Gupta <109612830+ShubhanshuGupta3011@users.noreply.github.com>
2026-02-09 01:11:29 +0530

    Time: 1 ms (99.31%) | Memory: 17.6 MB (11.46%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -1,27 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    int findNumberOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n,1);
-        vector<int> cnt(n,1);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i] <= nums[j]) continue;
-                if(dp[i] < 1+dp[j]){
-                    dp[i] = 1+dp[j];
-                    cnt[i] = cnt[j];
-                }else if(dp[i] == (1+dp[j])){
-                    cnt[i] += cnt[j];
+    struct BIT {
+        int size;
+        vector<int> lic;  
+        vector<int> cnt;   
+
+        BIT(int n) {
+            size = n + 1;
+            lic.assign(size, 0);
+            cnt.assign(size, 0);
+        }
+
+        void update(int idx, int length, int count) {
+            idx++; 
+            while (idx < size) {
+                if (lic[idx] == length) {
+                    cnt[idx] += count;
+                } else if (lic[idx] < length) {
+                    lic[idx] = length;
+                    cnt[idx] = count;
                 }
+                idx += idx & -idx;
             }
         }
-        int maxi = *max_element(dp.begin(),dp.end());
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            if(dp[i] == maxi){
-                ans += cnt[i];
+
+        pair<int,int> query(int idx) {
+            int length = 0;
+            int count = 0;
+            idx++;  
+            while (idx > 0) {
+                if (lic[idx] == length) {
+                    count += cnt[idx];
+                } else if (lic[idx] > length) {
+                    length = lic[idx];
+                    count = cnt[idx];
+                }
+                idx -= idx & -idx;
             }
+            return {length, count};
         }
-        return ans;
+    };
+
+    int findNumberOfLIS(vector<int>& nums) {
+        vector<int> temp = nums;
+        sort(temp.begin(), temp.end());
+        temp.erase(unique(temp.begin(), temp.end()), temp.end());
+        int n = temp.size();
+        BIT bit(n);
+        int maxLen = 0;
+        int total = 0;
+        for (int num : nums) {
+            int idx = lower_bound(temp.begin(), temp.end(), num) - temp.begin();
+            auto [len, count] = bit.query(idx - 1);
+            len += 1; 
+            if (len == 1) count = 1;
+            if (len == maxLen) {
+                total += count;
+            } else if (len > maxLen) {
+                maxLen = len;
+                total = count;
+            }
+            bit.update(idx, len, count);
+        }
+        return total;
     }
 };
