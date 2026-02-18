e9612cb344a980331e38eee3dc6f2d52631a6cba
Shubhanshu Gupta <109612830+ShubhanshuGupta3011@users.noreply.github.com>
2026-02-19 03:56:21 +0530

    Time: 5 ms (97.31%) | Memory: 24.7 MB (41.36%) - LeetSync

--- 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.cpp
+++ 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    vector<int> fact;
+    vector<int> count2;
+    vector<int> count5;
+    vector<int> inv2 = {0,1,0,7,0,0,0,3,0,9};
+    vector<int> power2 = {6,2,4,8};
     int mod = 10;
     int multi(int a,int b){
         return (a*b) % mod;
@@ -7,20 +12,40 @@ public:
     int add(int a,int b){
         return (a+b) % mod;
     }
-    int nCr(int n,int r,vector<vector<int>>& dp){
-        if(n <= r) return (n == r);
-        if(r==0) return 1;
-        if(dp[n][r] != -1) return dp[n][r];
-        return dp[n][r] = add(nCr(n-1,r,dp) , nCr(n-1,r-1,dp));
+    int nCr(int n,int r){
+        int two = count2[n] - count2[r] - count2[n-r];
+        int five = count5[n] - count5[r] - count5[n-r];
+        if(two && five) return 0;
+        int ans = fact[n];
+        ans = multi(ans,inv2[fact[r]]);
+        ans = multi(ans,inv2[fact[n-r]]);
+        return two ? multi(ans,power2[two & 3]): (five ? multi(ans,5) : ans) ;
     }
     int triangularSum(vector<int>& nums) {
         int n = nums.size();
+        fact.resize(n,1);
+        count2.resize(n,0);
+        count5.resize(n,0);
+        for(int i=1;i<n;i++){
+            int value = i;
+            int two = 0;
+            int five = 0;
+            while(value%2 == 0){
+                value = value/2;
+                two++;
+            }
+            while(value%5 == 0){
+                value = value/5;
+                five++;
+            }
+            fact[i] = multi(fact[i-1],value);
+            count2[i] = two + count2[i-1];
+            count5[i] = five + count5[i-1];
+        }
         int ans = 0;
-        vector<vector<int>> dp(n,vector<int>(n,-1));
         for(int i=0;i<n;i++){
-            int combination = nCr(n-1,i,dp);
-            int value = nums[i];
-            ans = add(ans,multi(combination,value));
+            int comb = nCr(n-1,i);
+            ans = add(ans,multi(comb,nums[i]));
         }
         return ans;
     }
