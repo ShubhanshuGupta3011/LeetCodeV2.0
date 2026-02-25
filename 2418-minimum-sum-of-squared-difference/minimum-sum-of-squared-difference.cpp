78322b0f6abc6a3b1b75679bfd0dab07cc221658
Shubhanshu Gupta <109612830+ShubhanshuGupta3011@users.noreply.github.com>
2026-02-25 06:41:09 +0530

    Time: 2593 ms (5.18%) | Memory: 150.8 MB (15.03%) - LeetSync

---
+++ 2418-minimum-sum-of-squared-difference/minimum-sum-of-squared-difference.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    long long minSumSquareDiff(vector<int>& nums1, vector<int>& nums2, int k1, int k2) {
+        /*
+        1,4,10,12
+        5,8,6,9
+
+        4 4 4 3
+        */
+        int n = nums1.size();
+        int k = k1+k2;
+        map<int,int> mp;
+        for(int i=0;i<n;i++){
+            int key = -abs(nums1[i]-nums2[i]);
+            mp[key]++;
+        }
+        while(true){
+            if(k==0) break;
+            for(auto it:mp){
+                int key = it.first; 
+                int freq = it.second;
+                cout<<"key = "<<key<<" freq = "<<freq<<" k = "<<k<<endl;
+                if(key == 0) return 0;
+                if(freq >= k){
+                    mp[key] = freq-k;
+                    mp[1+key] += k;
+                    k = 0;
+                }else{
+                    mp[1+key] += freq;
+                    k -= freq;
+                    mp.erase(key);
+                }
+                break;
+            }
+        }
+        long long ans = 0;
+        for(auto it:mp){
+            long long key = it.first;
+            long long freq = it.second;
+            // cout<<"key = "<<key<<" freq = "<<freq<<endl;
+            ans += freq * (key * key);
+        }
+        return ans;
+    }
+};
