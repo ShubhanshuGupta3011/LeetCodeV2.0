498e23caeb0c9c8cad34056063e6d67c106dc7dd
Shubhanshu Gupta <109612830+ShubhanshuGupta3011@users.noreply.github.com>
2026-03-03 13:57:43 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (6.36%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int squareSum(int n){
+        int ans = 0;
+        while(n){
+            ans += (n%10) * (n%10);
+            n = n/10;
+        }
+        return ans;
+    }
+    bool helper(int n,vector<int>& dp){
+        if(n >= 730) return helper(squareSum(n),dp);
+        if(dp[n] != -1) return dp[n];
+        dp[n] = 0;
+        return dp[n] = helper(squareSum(n),dp);
+    }
+    bool isHappy(int n) {
+        vector<int> dp(730,-1);
+        dp[1] = 1;
+        return helper(n,dp);
+    }
+};
