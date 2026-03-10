0aa100562848308438839ff86da1b6d9067ae49b
Shubhanshu Gupta <109612830+ShubhanshuGupta3011@users.noreply.github.com>
2026-03-10 10:59:30 +0530

    Time: 168 ms (67.12%) | Memory: 94.7 MB (60.27%) - LeetSync

--- 3407-find-all-possible-stable-binary-arrays-ii/find-all-possible-stable-binary-arrays-ii.cpp
+++ 3407-find-all-possible-stable-binary-arrays-ii/find-all-possible-stable-binary-arrays-ii.cpp
@@ -2,32 +2,26 @@ class Solution {
 public:
     int mod = 1e9+7;
     int add(int a,int b){
-        if(a+b > mod) return a+b-mod;
-        return a+b;
+        return (a+b > mod) ? a+b-mod : a+b;
     }
     int minus(int a,int b){
-        if(a<b) return (a-b+mod);
-        return a-b;
+        return a<b ? a-b+mod : a-b;
     }
     int numberOfStableArrays(int zero, int one, int limit) {
         vector<vector<int>> dp0(1+zero,vector<int>(1+one,0));
         vector<vector<int>> dp1(1+zero,vector<int>(1+one,0));
-
         for(int i=0;i<=min(zero,limit);i++) dp0[i][0] = 1;
         for(int j=0;j<=min(one,limit);j++) dp1[0][j] = 1;
-
         for(int i=1;i<=zero;i++){
             for(int j=1;j<=one;j++){
                 dp0[i][j] = add(dp0[i-1][j],dp1[i-1][j]);
                 int gap = i - limit - 1;
                 if(gap>=0) dp0[i][j]=minus(dp0[i][j],dp1[gap][j]);
-
                 dp1[i][j] = add(dp1[i][j-1],dp0[i][j-1]);
                 gap = j - limit - 1;
                 if(gap>=0) dp1[i][j]=minus(dp1[i][j],dp0[i][gap]);
             }
         }
-
         return add(dp0[zero][one],dp1[zero][one]);
     }
 };
