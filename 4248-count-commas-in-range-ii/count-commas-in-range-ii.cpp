10ee38f25f010e45d74165f46417f009a1b746da
Shubhanshu Gupta <109612830+ShubhanshuGupta3011@users.noreply.github.com>
2026-03-15 12:54:21 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (54.64%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -1,25 +1,10 @@
 class Solution {
 public:
-    int getLog(long long n){
-        if(n<1000) return 0;
-        return 1+getLog(n/1000);
+    long long helper(long long n,long long low){
+        if(n<low) return 0;
+        return (n-low+1) + helper(n,1000 * low);
     }
     long long countCommas(long long n) {
-        vector<long long> arr(6,999);
-        for(int i=1;i<6;i++){
-            arr[i] = 1000 * arr[i-1];
-        }
-        for(int i=0;i<6;i++){
-            arr[i] = i * arr[i];
-        }
-        long long index = getLog(n);
-        cout<<log(n)/log(10)<<endl;
-        long long ans = 0;
-        for(int i=0;i<index;i++){
-            ans += arr[i];
-        }
-        long long low = pow(1000,index);
-        ans += (n-low+1) * index;
-        return ans;
+        return helper(n,1000);
     }
 };
