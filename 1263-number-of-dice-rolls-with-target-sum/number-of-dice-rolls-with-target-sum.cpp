b389772f92f2a2249ba4cc6366829b5ef0dbd389
Shubhanshu Gupta <109612830+ShubhanshuGupta3011@users.noreply.github.com>
2026-08-03 05:29:22 +0530

    Time: 3 ms (98.57%) | Memory: 9 MB (91.30%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     int mod = 1e9 + 7;
-    int K;
     int add(int a,int b){
         return (a+b)>=mod?(a+b-mod):(a+b);
     }
-    int helper(int n,int target,vector<vector<int>>& dp){
-        if(n==0) return !target;
-        if(dp[n][target] != -1) return dp[n][target];
-        int ans=0;
-        for(int i=1;i<=K;i++){
-            if(i>target) break;
-            ans=add(ans,helper(n-1,target-i,dp));
-        }
-        return dp[n][target] = ans;
-    }
     int numRollsToTarget(int n, int k, int target) {
         if(target<n || target>n*k) return 0;
-        vector<vector<int>> dp(n+1,vector<int>(target+1,-1));
-        K=k;
-        return helper(n,target,dp);
+
+        vector<int> dp(1+target,0);
+        vector<int> empty(1+target,0);
+        dp[0]=1;
+
+        while(n--){
+            vector<int> currDp = empty;
+            for(int i=1;i<=k;i++){
+                for(int j=0;j<=target;j++){
+                    if(j+i > target) break;
+                    currDp[j+i] = add(currDp[j+i],dp[j]);
+                }
+            }
+            dp = currDp;
+        }
+
+        return dp[target];
     }
 };
