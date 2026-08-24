da387d5833ec5b55f85cddfc577faee8eee12341
Shubhanshu Gupta <109612830+ShubhanshuGupta3011@users.noreply.github.com>
2026-08-24 13:01:29 +0530

    Time: 62 ms (71.43%) | Memory: 272.5 MB (7.14%) - LeetSync

--- 4383-find-all-numbers-disappeared-in-an-array-ii/find-all-numbers-disappeared-in-an-array-ii.cpp
+++ 4383-find-all-numbers-disappeared-in-an-array-ii/find-all-numbers-disappeared-in-an-array-ii.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> findDisappearedNumbers(vector<int>& nums, int lower, int upper) {
-        unordered_set<int> st;
-        for(auto it:nums){
-            st.insert(it);
-        }
-        while(st.count(lower)){
-            lower++;
-        }
-        while(st.count(upper)){
-            upper--;
-        }
-        if(lower > upper) return {};
         vector<int> arr(1+upper,1);
-        for(auto it:st){
-            if(lower<it && it<upper){
-                arr[it] = 0;
-            }
+        arr[0] = 0;
+        for(auto it:nums){
+            if(it > upper) continue;
+            arr[it]=0;
         }
-        for(int i=lower+1;i<=upper;i++){
-            if(arr[i]){
-                arr[i] = 1+arr[i-1];
-            }
+        for(int i=1;i<=upper;i++){
+            if(arr[i] == 0) continue;
+            arr[i] = 1+arr[i-1];
         }
-
         vector<vector<int>> ans;
         for(int i=upper;i>=lower;i--){
-            if(arr[i] == 0) continue;
-            ans.push_back({i-arr[i]+1,i});
-            i -= arr[i];
+            if(arr[i]==0) continue;
+            ans.push_back({max(i-arr[i]+1,lower),i});
+            i-=arr[i];
         }
         reverse(ans.begin(),ans.end());
         return ans;
